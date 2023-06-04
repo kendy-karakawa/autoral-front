29102cee89064617a59530a6424ebbd2ca4ac754
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-04 19:00:53 -0300

    feat: connect to API

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
-    return (
-      <h1 >
-        Estou na home
-      </h1>
-    )
-  }
+  const [test, setTest] = useState("");
+  useEffect(() => {
+    async function feach() {
+      try {
+        const data = await axios.get(
+          `${import.meta.env.VITE_APP_API_URL}/test`
+        );
+        setTest(data.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    feach()
+  }, []);
+
+  return <h1>Estou na home {test}</h1>;
+}
---
+++ src/services/ApiAuth.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+import axios from "axios";
+
+function signUp(body) {
+  return axios.post(`${import.meta.env.VITE_APP_API_URL}/user`, body);
+}
+
+function signIn(body) {
+  return axios.post(`${import.meta.env.VITE_APP_API_URL}/sigin`, body);
+}
+
+function signOut(token) {
+  axios.delete(`${import.meta.env.VITE_APP_API_URL}/sigin`, createConfig(token));
+}
+
+const apiAuth = {
+  signUp,
+  signIn,
+  signOut,
+};
+
+export default apiAuth;
