fd4792b92a4b1d7a5ebbb4e2215b5b4a3f8e04b0
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-10 17:18:29 -0300

    feat status in userCard

--- src/components/Card/UserCard.jsx
+++ src/components/Card/UserCard.jsx
@@ -1,17 +1,20 @@
 import { w } from "windstitch";
 
 export default function UserCard({ data }) {
-  const { userId, User } = data;
+  const { userId, accepted, User } = data;
   const { name, image } = User;
 
   return (
     <CardContainer>
-      <Div>
+      <Box>
         <Img src={image} />
         <Div>
           <UserName>{name}</UserName>
+          <Text>
+            {accepted ? "Membro" : "Aguardando confirmação"}
+          </Text>
         </Div>
-      </Div>
+      </Box>
     </CardContainer>
   );
 }
@@ -26,9 +29,17 @@ w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500
 `);
 
 const Div = w.div(`
+  flex flex-col ml-2
+`)
+
+const Box = w.div(`
 flex items-center 
 `);
 
 const UserName = w.p(`
-text-1xl font-semibold text-gray-900 dark:text-white ml-2
+text-1xl font-semibold text-gray-900 dark:text-white 
 `);
+
+const Text = w.p(`
+  text-xs font-normal text-gray-900
+`)
